37d118a3bc8ac876abede615a39fff3f09d3e5ca
fredericVDK <fredericvanderkeilen@yahoo.com>
2026-07-17 14:16:06 +0200

    Added Not Nullable

--- tutorial/estate/models/estate_property.py
+++ tutorial/estate/models/estate_property.py
@@ -5,12 +5,12 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
 
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
 
     bedrooms = fields.Integer()
