cecb0e0b971f60d6aaccd0634c303c5cb3134102
fredericVDK <fredericvanderkeilen@yahoo.com>
2026-07-18 14:20:51 +0200

    Added Actions, menu's and some field adjustments

--- tutorial/estate/__manifest__.py
+++ tutorial/estate/__manifest__.py
@@ -3,5 +3,9 @@
 {
     "name": "Estate",
     "depends": ["base"],
-    "data": ["security/ir.model.access.csv"],    
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml"
+        ],    
 }
