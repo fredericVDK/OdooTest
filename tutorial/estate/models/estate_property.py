74561e0c1424a75ad0ce4616048daad76def0198
fredericVDK <fredericvanderkeilen@yahoo.com>
2026-07-18 15:08:37 +0200

    Added Property type, buyer and Tags

--- tutorial/estate/models/estate_property.py
+++ tutorial/estate/models/estate_property.py
@@ -12,13 +12,13 @@ class EstateProperty(models.Model):
     date_availability = fields.Date(
     copy=False,
     default=lambda self: fields.Date.today() + relativedelta(months=3),
-)
+    )
 
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(
     readonly=True,
     copy=False,
-)
+    )
 
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
@@ -50,4 +50,32 @@ class EstateProperty(models.Model):
         required=True,
         copy=False,
         default="new",
+    )
+    
+    property_type_id = fields.Many2one(
+    "estate.property.type",
+    string="Property Type",
+    )
+
+    buyer_id = fields.Many2one(
+        "res.partner",
+        string="Buyer",
+        copy=False,
+    )
+
+    salesperson_id = fields.Many2one(
+        "res.users",
+        string="Salesperson",
+        default=lambda self: self.env.user,
+    )
+
+    tag_ids = fields.Many2many(
+        "estate.property.tag",
+        string="Tags",
+    )
+
+    offer_ids = fields.One2many(
+        "estate.property.offer",
+        "property_id",
+        string="Offers",
     )
