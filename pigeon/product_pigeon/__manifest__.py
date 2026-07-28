94fe317571162aa9e1b059115839588d40501bc9
fredericVDK <fredericvanderkeilen@yahoo.com>
2026-07-28 14:52:58 +0200

    base setup pigeon folder

---
+++ pigeon/product_pigeon/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Product Pigeon",
+    "version": "19.0.1.0.0",
+    "summary": "Import pigeon breeds as products from Wikimedia APIs",
+    "author": "Frederic",
+    "license": "LGPL-3",
+    "depends": ["product"],
+    "installable": True,
+}
